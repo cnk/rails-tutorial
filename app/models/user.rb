87cdd316cfb2599eb77d948340618f38e6399dce
Cynthia Kiser <cnk@ugcs.caltech.edu>
2011-09-18 19:27:34 -0700

    add password fields without encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessor :password
   attr_accessible :name, :email
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -19,4 +20,21 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format   => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+  
+  before_save :encrypt_password
+  
+  private
+  
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      string # Only a temporary implementation!
+    end
+
 end
