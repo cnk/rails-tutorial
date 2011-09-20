2c77a21b6a436929ff70031c154bdb36f18a6fbb
Cynthia Kiser <cnk@ugcs.caltech.edu>
2011-09-20 14:45:46 -0700

    start of session stuff

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+    @title = "Sign in"
+  end
+
+  def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      # Create error messages and rerender new
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign in"
+      render 'new'
+    else
+      # set session cookie and redirect user to his own page
+      sign_in user
+      redirect_to user
+    end
+  end
+
+  def destroy
+  end
+
+end
