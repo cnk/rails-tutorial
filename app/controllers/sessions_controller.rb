8c53fcca42259d20be5391df0b6201387e7b3c7f
Cynthia Kiser <cnk@ugcs.caltech.edu>
2011-09-24 07:53:45 -0700

    better page flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,9 +12,9 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      # set session cookie and redirect user to his own page
+      # set session cookie and redirect user back to where he started OR to his own page
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
 
