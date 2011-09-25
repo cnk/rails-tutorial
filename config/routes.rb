86c6a1a4fd17264fad18e66c7d8409c08e9770b5
Cynthia Kiser <cnk@ugcs.caltech.edu>
2011-09-25 00:52:15 -0700

    we can now add microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:new, :create]
 
   # some aliases for stock RESTful routes
   match '/signup',  :to => 'users#new'
