3717ee5f714b60cdc241a751fc2c39c7c616df3d
Cynthia Kiser <cnk@ugcs.caltech.edu>
2011-09-24 23:18:29 -0700

    microposts displayed on profile

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.10'
 gem 'gravatar_image_tag'
+gem 'will_paginate'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts
+    @microposts = @user.microposts.paginate(:page => params[:page], :per_page => 3)
     @title = "#{@user.name}'s page"
   end
 
